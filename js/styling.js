272050004a57b79710b3cce8674ff8d117f69f9f
CALEB FONYUY ASHERI <Calebfonyuy@users.noreply.github.com>
2019-03-29 20:52:04 +0100

    update

--- js/styling.js
+++ js/styling.js
@@ -1,33 +1,31 @@
 function loadHeader() {
-	var req;
-	var doc = "header.html";
-	if(window.XMLHttpRequest){
-		req=new XMLHttpRequest();
-	}else{
-		req= new ActiveObject("Microsoft.XMLHTTP");
+	var req, doc = "header.html";
+	if (window.XMLHttpRequest) {
+		req = new XMLHttpRequest();
+	} else {
+		req = new ActiveObject("Microsoft.XMLHTTP");
 	}
-	req.onreadystatechange=function () {
-		if (req.readyState==4) {
-			document.querySelector(".header").innerHTML=req.responseText;
+	req.onreadystatechange = function () {
+		if (req.readyState === 4) {
+			document.querySelector(".header").innerHTML = req.responseText;
 		}
-	}
-	req.open("GET",doc,true);
+	};
+	req.open("GET", doc, true);
 	req.send();
 }
 
 function loadFooter() {
-	var req;
-	var doc = "footer.html";
-	if(window.XMLHttpRequest){
-		req=new XMLHttpRequest();
-	}else{
-		req= new ActiveObject("Microsoft.XMLHTTP");
+	var req, doc = "footer.html";
+	if (window.XMLHttpRequest) {
+		req = new XMLHttpRequest();
+	} else {
+		req = new ActiveObject("Microsoft.XMLHTTP");
 	}
-	req.onreadystatechange=function () {
-		if (req.readyState==4) {
-			document.querySelector(".footer").innerHTML=req.responseText;
+	req.onreadystatechange = function () {
+		if (req.readyState === 4) {
+			document.querySelector(".footer").innerHTML = req.responseText;
 		}
-	}
-	req.open("GET",doc,true);
+	};
+	req.open("GET", doc, true);
 	req.send();
 }
